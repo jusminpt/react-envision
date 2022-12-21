4e54ae68d0aa72e1c0ebe6a2baa757db4480863a
jmjusmin <jmjusmin1412@gmail.com>
2022-12-21 14:44:38 -0800

    Fix dropdown when clicked

--- src/components/Navbar/DropDown.jsx
+++ src/components/Navbar/DropDown.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { BsArrowRightShort } from "react-icons/bs";
 
 function DropDown({ sublinks }) {
   const [click, setClick] = useState(false);
@@ -18,10 +17,13 @@ function DropDown({ sublinks }) {
         {sublinks.map((sublink, index) => {
           return (
             <li key={index}>
-              {/* TODO: Close the dropdown when click */}
               <Link
                 to={sublink.url}
-                className="app__navbar-dropdown-links"
+                className={
+                  click
+                    ? "app__navbar-dropdown-links clicked"
+                    : "app__navbar-dropdown-links"
+                }
                 onClick={() => setClick(false)}
               >
                 {sublink.title}
