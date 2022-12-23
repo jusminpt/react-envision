a4933b82024ddc70c24cc843c6e3c07862072dbe
jmjusmin <jmjusmin1412@gmail.com>
2022-12-22 21:37:36 -0800

    Responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,55 +16,59 @@ function Footer() {
           <Button textInput="Donate" styleBtn="secondary" />
         </div>
         <div className="app__footer-links">
-          <div>
-            <h3>Navigation</h3>
-            <ul>
-              {linksData.map((item, index) => (
-                <li key={`link-${index}`}>
-                  <Link to={`${item.url}`}>{item.title}</Link>
+          <div className="app__footer-links-left">
+            <div>
+              <h3>Navigation</h3>
+              <ul>
+                {linksData.map((item, index) => (
+                  <li key={`link-${index}`}>
+                    <Link to={`${item.url}`}>{item.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h3>What We Do</h3>
+              <ul>
+                {/* TODO : find a better way to map */}
+                {linksData.map(
+                  (item) =>
+                    item.title === "What We Do" &&
+                    item.sublink.map((item, index) => (
+                      <li key={`link-${index}`}>
+                        <Link to={`${item.url}`}>{item.title}</Link>
+                      </li>
+                    ))
+                )}
+              </ul>
+            </div>
+          </div>
+          <div className="app__footer-links-right">
+            <div>
+              <h3>About Us</h3>
+              <ul>
+                {/* TODO : find a better way to map */}
+                {linksData.map(
+                  (item) =>
+                    item.title === "About Us" &&
+                    item.sublink.map((item, index) => (
+                      <li key={`link-${index}`}>
+                        <Link to={`${item.url}`}>{item.title}</Link>
+                      </li>
+                    ))
+                )}
+              </ul>
+            </div>
+            <div>
+              <h3>Talk to Us</h3>
+              <ul>
+                <li>
+                  655 Morris Avenue STE 2 <br />
+                  The Bronx NY 10451 US
                 </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h3>What We Do</h3>
-            <ul>
-              {/* TODO : find a better way to map */}
-              {linksData.map(
-                (item) =>
-                  item.title === "What We Do" &&
-                  item.sublink.map((item, index) => (
-                    <li key={`link-${index}`}>
-                      <Link to={`${item.url}`}>{item.title}</Link>
-                    </li>
-                  ))
-              )}
-            </ul>
-          </div>
-          <div>
-            <h3>About Us</h3>
-            <ul>
-              {/* TODO : find a better way to map */}
-              {linksData.map(
-                (item) =>
-                  item.title === "About Us" &&
-                  item.sublink.map((item, index) => (
-                    <li key={`link-${index}`}>
-                      <Link to={`${item.url}`}>{item.title}</Link>
-                    </li>
-                  ))
-              )}
-            </ul>
-          </div>
-          <div>
-            <h3>Talk to Us</h3>
-            <ul>
-              <li>
-                655 Morris Avenue STE 2 <br />
-                The Bronx NY 10451 US
-              </li>
-              <li>Tel 646-589-2589</li>
-            </ul>
+                <li>Tel 646-589-2589</li>
+              </ul>
+            </div>
           </div>
         </div>
         <hr className="app__footer-line" />
