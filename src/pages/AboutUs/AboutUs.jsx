4b3f9523aaf8d4b10240723139a7334d8f269ca7
jmjusmin <jmjusmin1412@gmail.com>
2023-03-05 17:00:25 -0800

    Update AboutUs style and h1 global style

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./AboutUs.scss";
 import { PageHero } from "../../components";
 
 function AboutUs() {
@@ -16,30 +17,35 @@ function AboutUs() {
           research institutions.
         </p>
         {/* TODO: Style the section */}
-        <div className="">
-          <div className="">
-            <h3>Mission</h3>
-            <p>
-              We promote healthy lives in the world’s impoverished communities,
-              work to improve longevity for people living with HIV/AIDS, and
-              work to reduce the social stigma, transmission, and HIV-related
-              morbidity and mortality by innovative methods.
-            </p>
+        <div className="mission-container">
+          <div className="mission-div-50">
+            <div className="mission-div-50-content">
+              <h3>Mission</h3>
+              <p>
+                We promote healthy lives in the world’s impoverished
+                communities, work to improve longevity for people living with
+                HIV/AIDS, and work to reduce the social stigma, transmission,
+                and HIV-related morbidity and mortality by innovative methods.
+              </p>
+            </div>
+            <div className="mission-div-50-content">
+              <h3>Vision</h3>
+              <p>
+                A world without fear of HIV/AIDS, reduced social stigma and
+                improved the quality of life for people living in the world’s
+                most impoverished communities.
+              </p>
+            </div>
           </div>
-          <div>
-            <h3>Vision</h3>
-            <p>
-              A world without fear of HIV/AIDS, reduced social stigma and
-              improved the quality of life for people living in the world’s most
-              impoverished communities.
-            </p>
-          </div>
-          <div>
-            <h3>Values</h3>
-            <p>
-              Our core values are an integrated public health workforce,
-              excellence, innovation, compassion, and respect for human dignity.
-            </p>
+          <div className="mission-div-100">
+            <div>
+              <h3>Values</h3>
+              <p>
+                Our core values are an integrated public health workforce,
+                excellence, innovation, compassion, and respect for human
+                dignity.
+              </p>
+            </div>
           </div>
         </div>
         <div className="aboutUs-teamMember">
