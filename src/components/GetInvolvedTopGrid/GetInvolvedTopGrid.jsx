1123e9fcbf22855db1cc2d5d0e3c69fb78130271
Malcolm <malcolmharrellmedia@gmail.com>
2023-03-25 13:55:09 -0700

    add top grid to GetInvolved page

---
+++ src/components/GetInvolvedTopGrid/GetInvolvedTopGrid.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import './GetInvolvedTopGrid.scss'
+import Button from "../Button/Button";
+
+import housesImg from "../../assets/img/image 8.png";
+
+
+
+
+
+
+
+
+
+
+function TopGrid ({}) {
+
+  return (
+    <div className="image-grid-container">
+
+<h2 className="title">Empower communities with your donation - help us improve, educate and help those in need!</h2>
+      <div className="image-grid-left">
+
+        <div className="img-container">
+          <img src={housesImg} alt="" />
+          <div className="btn-container">
+            <Button
+              textInput="Donate"
+              styleBtn="secondary"
+              urlBtn="/whatwedo"
+            />
+
+          </div>
+          
+
+        </div>
+      </div>
+      <div className="image-grid-right">
+        
+        <img src={housesImg} alt="" />
+      </div>
+    </div>
+  );
+    
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function TopGrid ({}) {
+
+//     return (
+//     <div class="grid-container">
+//         <div class="title">Empower communities with your donation - help us improve, educate and help those in need!</div>
+
+//         <div class="grid-item">
+//             <img className="img-style image-02" src={housesImg} alt=""  />
+//         </div>
+
+//         <div class="grid-item">
+
+//           <div className="btn-style">
+//                   <Button
+//                         textInput="Donate"
+//                         styleBtn="secondary"
+//                         urlBtn="/whatwedo"
+//                       />
+//           </div>
+
+//           <img className="img-style image-01" src={housesImg} alt=""   />
+//         </div>
+
+//   </div>
+//     );
+// }
+
+export default TopGrid
--- src/pages/GetInvolved/GetInvolved.jsx
+++ src/pages/GetInvolved/GetInvolved.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { PageHero } from "../../components";
+import TopGrid from "../../components/GetInvolvedTopGrid/GetInvolvedTopGrid.jsx";
+import './GetInvolved.scss'
 
 function GetInvolved() {
   return (
+
+    
     <div>
       <PageHero headingText="Get Involved" />
+
+      <div className="main-container">
+        
+        <h1 className="involved-title">Ways to Support Us</h1>
+        <p>Your contribution will significantly improve the lives of the people in Liberia, Malawi, and Tanzania by providing them access to education, healthcare, and sustainable development initiatives. Additionally, you can also consider volunteering your time and skills to support Envision's initiatives and make a positive difference in the world.</p>
+
+
+        <div className="top-grid">
+        <TopGrid />
+        </div>
+
+      </div>
+    
     </div>
   );
 }
