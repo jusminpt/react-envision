88e4ec3d3aebbce8855dc6e945e9f5e589859fa2
jmjusmin <jmjusmin1412@gmail.com>
2023-04-20 13:20:36 -0700

    Remove content and update banner resources

--- src/pages/GetInvolved/GetInvolved.jsx
+++ src/pages/GetInvolved/GetInvolved.jsx
@@ -1,47 +1,49 @@
 import React from "react";
 import { PageHero } from "../../components";
-import './GetInvolved.scss';
-
 import TopGrid from "../../components/GetInvolvedTopGrid/GetInvolvedTopGrid.jsx";
 import GetInvolvedCard from "../../components/GetInvolvedCard/GetInvolvedCard";
 import GetInvolvedFlexCard from "../../components/GetInvolvedFlexCard/GetInvolvedFlexCard";
-
-import bannerImg from "../../assets/img/banner-resources.png";
+import bannerGetinvolved from "../../assets/img/banner-getinvolved.png";
+import "./GetInvolved.scss";
 
 function GetInvolved() {
   return (
     <div>
-      <PageHero headingText="Get Involved"  img={bannerImg} />
+      <PageHero headingText="Get Involved" img={bannerGetinvolved} />
 
       <div className="page-section">
-        
         <h2 className="involved-title">Ways to Support Us</h2>
-        <p className="involved-p">Your contribution will significantly improve the lives of the people in Liberia, Malawi, and Tanzania by providing them access to education, healthcare, and sustainable development initiatives. Additionally, you can also consider volunteering your time and skills to support Envision's initiatives and make a positive difference in the world.</p>
+        <p className="involved-p">
+          Your contribution will significantly improve the lives of the people
+          in Liberia, Malawi, and Tanzania by providing them access to
+          education, healthcare, and sustainable development initiatives.
+          Additionally, you can also consider volunteering your time and skills
+          to support Envision's initiatives and make a positive difference in
+          the world.
+        </p>
 
-       <div className="top-grid">
-          < TopGrid />
-        </div> 
+        <div className="top-grid">
+          <TopGrid />
+        </div>
 
         <div className="middle-card">
-          < GetInvolvedCard
-              textInput="Join us in making a difference in the world by lending your support to our cause."
-              textBtn="See Document"
-              urlBtn="/"
-            />
-        </div> 
+          <GetInvolvedCard
+            textInput="Join us in making a difference in the world by lending your support to our cause."
+            textBtn="See Document"
+            urlBtn="/"
+          />
+        </div>
 
-         <div className="end-card">
-          < GetInvolvedFlexCard
+        <div className="end-card">
+          <GetInvolvedFlexCard
             textInputOne="More People"
             textInputTwo="More Impact"
-            pInput = "Together, we can create a healthier world with Envision Health Access Initiative."
+            pInput="Together, we can create a healthier world with Envision Health Access Initiative."
             textBtn="Join Us"
             urlBtn="/"
           />
-        </div> 
-
+        </div>
       </div>
-    
     </div>
   );
 }
--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -12,20 +12,6 @@ function Resources() {
     <div>
       <PageHero headingText="Resources" img={bannerResources} />
       <div className="page-section">
-        {/* TODO: Wait for updated paragraph */}
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-          vulputate libero et velit interdum, ac aliquet odio mattis. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Curabitur tempus urna at turpis condimentum
-          lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis
-          condimentum ac, vestibulum eu nisl.226226Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Nunc vulputate libero et velit interdum,
-          ac aliquet odio mattis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus
-          urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut
-          diam quam, semper iaculis condimentum ac, vestibulum eu nisl.226226
-        </p>
         <div className="resourcesCard-section">
           {/* TODO: find nested route solution */}
           <ResourceCard resourcesData={resourcesData[0]} img={covidPic} />
