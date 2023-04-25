f2ae802d3e5a43d8a1056991e7cbbd309b3a9264
jmjusmin <jmjusmin1412@gmail.com>
2023-04-24 22:28:48 -0700

    Update style Resources page

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PageHero, ResourceCard } from "../../components";
 import bannerResources from "../../assets/img/banner-resources.png";
 import resourcesData from "../../data/resourcesData";
@@ -6,10 +6,22 @@ import "./Resources.scss";
 import "../Pages.scss";
 
 function Resources() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 980);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        const ismobile = window.innerWidth < 980;
+        if (ismobile !== isMobile) setIsMobile(ismobile);
+      },
+      false
+    );
+  }, [isMobile]);
   return (
     <div>
       <PageHero headingText="Resources" img={bannerResources} />
-      <div className="page-section">
+      <div className={!isMobile ? "page-section" : null}>
         <div className="resourcesCard-section">
           {resourcesData.map((resourceData) => (
             <ResourceCard key={resourceData.id} resourcesData={resourceData} />
